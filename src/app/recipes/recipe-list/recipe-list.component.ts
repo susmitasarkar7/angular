d5e748e05d309e968141cc03d9eb77cc2f2a1ce5
susmita sarkar <sarkarsusmita253@gmail.com>
2018-10-31 18:21:50 +0530

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test',
+     'https://get.pxhere.com/photo/dish-meal-food-cooking-vegetable-recipe-eat-barbecue-cuisine-chicken-dining-platter-vegetarian-food-dining-room-side-dish-stuffing-cookware-and-bakeware-fusion-cuisine-1373210.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test',
+     'https://get.pxhere.com/photo/dish-meal-food-cooking-vegetable-recipe-eat-barbecue-cuisine-chicken-dining-platter-vegetarian-food-dining-room-side-dish-stuffing-cookware-and-bakeware-fusion-cuisine-1373210.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
