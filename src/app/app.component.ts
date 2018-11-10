6430096bc1371199674649bf4ee38d51f2b3446f
susmita sarkar <sarkarsusmita253@gmail.com>
2018-11-10 17:52:20 +0530

    authentication and routing protection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDNhZxgmInbi8pUR46VQ7aMLhYEU3PSeS8",
+      authDomain: "my-recipe-book-j397.firebaseapp.com"
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,38 +1,43 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import 'rxjs/Rx';
 
-import { RecipeService } from "../recipes/recipe.service";
+import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import 'rxjs/add/operator/map';
+import { AuthService } from '../auth/auth.service';
 
-@Injectable ()
+@Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) {}
+  constructor(private http: Http, 
+              private recipeService: RecipeService,
+              private authService: AuthService) {}
 
-    storeRecipes() {
-        return this.http.put('https://my-recipe-book-j397.firebaseio.com/recipes.json',
-        this.recipeService.getRecipes()
-        );
-    }
+  storeRecipes() {
+    const token =  this.authService.getToken()
 
-    getRecipes() {
-        this.http.get('https://my-recipe-book-j397.firebaseio.com/recipes.json')
-            .map(
-                (response : Response) => {
-                    const recipes: Recipe[] = response.json();
-                    for (let recipe of recipes) {
-                        if (!recipe['ingredients']) {
-                            console.log(recipe);
-                            recipe['ingredients'] = [];
-                        }
-                    }
-                return recipes;
-                }
-            )
-            .subscribe(
-                (recipes : Recipe[]) => {
-                    this.recipeService.setRecipes(recipes);
-                }
-            );
-    }
+    return this.http.put('https://my-recipe-book-j397.firebaseio.com/recipes.json?auth=' + token , 
+    this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+   const token =  this.authService.getToken()
+      
+    this.http.get('https://my-recipe-book-j397.firebaseio.com/recipes.json?auth=' + token)
+      .map(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipeService.setRecipes(recipes);
+        }
+      );
+  }
 }
